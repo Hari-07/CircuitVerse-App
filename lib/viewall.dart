14833a2b4fe409823f180199e152a92c48f22fd2
Harinandan <harinandan.alukkal@gmail.com>
2020-03-31 14:00:22 +0530

    Pagination with scroll

--- lib/viewall.dart
+++ lib/viewall.dart
@@ -10,137 +10,131 @@ int page = 1;
 bool full = false;
 
 class Attributes{
-  final String name;
+	final String name;
 
-  Attributes({
-    this.name,
-  });
+	Attributes({
+		this.name,
+	});
 
-  factory Attributes.fromJson(Map<String, dynamic> json3) {
-    return Attributes(
-      name: json3['name']
-    );
-  }
+	factory Attributes.fromJson(Map<String, dynamic> json3) {
+		return Attributes(
+			name: json3['name']
+		);
+	}
 }
 
 class Project {
-  String id;
-  String type;
-  Attributes attributes;
+	String id;
+	String type;
+	Attributes attributes;
 
-  Project({
-    this.id,
-    this.type,
-    this.attributes,
-  });
+	Project({
+		this.id,
+		this.type,
+		this.attributes,
+	});
 
-  factory Project.fromJson(Map<String, dynamic> json2) {
-    return Project(
-      id : json2['id'],
-      type : json2['type'],
-      attributes : Attributes.fromJson(json2['attributes'])
-    );
-  }
+	factory Project.fromJson(Map<String, dynamic> json2) {
+		return Project(
+			id : json2['id'],
+			type : json2['type'],
+			attributes : Attributes.fromJson(json2['attributes'])
+		);
+	}
 }
 
 class Data {
-  List<Project> projects;
+	List<Project> projects;
 
-  Data({
-    this.projects,
-  });
+	Data({
+		this.projects,
+	});
 
-  factory Data.fromJson(Map<String, dynamic> json) {
-    var list  = json['data'] as List;
-  
-    List<Project> projectsList = list.map((i) => Project.fromJson(i)).toList();
-    print(projectsList);
-    if(projectsList.isEmpty)
-      full = true;
-    P.addAll(projectsList);
-    return Data(
-      projects : P,
-    );
-  }
+	factory Data.fromJson(Map<String, dynamic> json) {
+		var list  = json['data'] as List;
+	
+		List<Project> projectsList = list.map((i) => Project.fromJson(i)).toList();
+		print(projectsList);
+		if(projectsList.isEmpty)
+			full = true;
+		P.addAll(projectsList);
+		return Data(
+			projects : P,
+		);
+	}
 }
 
 
 Data dataFromJson (String str) {
-  final  jsonData = json.decode(str);
-  return Data.fromJson(jsonData);
+	final  jsonData = json.decode(str);
+	return Data.fromJson(jsonData);
 }
 
 Future<Data> getData() async {
-  final response = await http.get('$url?page=$page');
-  return dataFromJson(response.body);
+	final response = await http.get('$url?page=$page');
+	return dataFromJson(response.body);
 }
 
 
 
 class AllProjects extends StatefulWidget {  
 
-  @override
-  _AllProjectsState createState() {
-    page = 1;
-    return _AllProjectsState();
-  }
+	@override
+	_AllProjectsState createState() {
+		page = 1;
+    P = [];
+		return _AllProjectsState();
+	}
 }
 
 class _AllProjectsState extends State<AllProjects> {
-  
-  ScrollController sc;
-  
-  void initstate() {
-    sc = ScrollController();
-    sc.addListener(scListener);
-  }
-
-  scListener() {
-    print("Changed");
-    if( sc.offset >= sc.position.maxScrollExtent && !sc.position.outOfRange) {
-      setState(() {
-        print("Reached Bottom");
-      });
-    }
-  }
-
+	
+	ScrollController sc;
+	
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Project Info'),
-      ),
-      backgroundColor: Colors.white,
-      body:FutureBuilder<Data>(
-        future: getData(),
-        builder: (context, snapshot) {
-          
-          if(snapshot.hasData)
-            return ListView.builder(
-              controller: sc,
-              itemCount: snapshot.data.projects.length,
-              itemBuilder: (context,index) {
-                return ListTile(
-                  title: Text('${snapshot.data.projects[index].attributes.name}',style: TextStyle(fontSize: 30),),
-                );
-              }
-            );
-          else
-            return Text('NotFound');
-        }
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if(!full) {
-            setState(() {
-              page+=1;
-            }); 
-          }
-        },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
+	void initState() {
+		sc = ScrollController();
+		super.initState();
+		sc.addListener(scListener);
+	}
+
+	scListener() {
+		if( sc.offset >= sc.position.maxScrollExtent && !sc.position.outOfRange) {
+			if(!full) {
+				setState(() {
+        page+=1;
+        }); 
+			}
+		}
+	}
+
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(
+				title: Text('Project Info'),
+			),
+			backgroundColor: Colors.white,
+			body:FutureBuilder<Data>(
+				future: getData(),
+				builder: (context, snapshot) {
+					
+					if(snapshot.hasData)
+						return ListView.builder(
+							controller: sc,
+							itemCount: snapshot.data.projects.length,
+							itemBuilder: (context,index) {
+								return ListTile(
+									title: Text('${snapshot.data.projects[index].attributes.name}',style: TextStyle(fontSize: 30),),
+								);
+							}
+						);
+					else
+						return Text('NotFound');
+				}
+			),
+		);
+	}
 }
 
 
